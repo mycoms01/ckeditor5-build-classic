2ee519fdbc671abfcae29abb175097126e9a26d1
sanit keawtawan <sanit.jigsaw@gmail.com>
2019-06-24 10:09:00 +0700

    add highlight

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,6 +28,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -53,7 +54,8 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Highlight
 ];
 
 // Editor configuration.
@@ -66,6 +68,7 @@ ClassicEditor.defaultConfig = {
 			'italic',
 			'link',
 			'alignment',
+			'highlight',
 			'bulletedList',
 			'numberedList',
 			'imageUpload',
